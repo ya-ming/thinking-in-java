5c9efc8bdb31bfb823f89c697cdd64c4c7f28472
YaMing Wu <wuyaming@gmail.com>
2018-11-19 11:47:52 +0800

    redo package move

---
+++ src/main/java/io/DirList.java
@@ -0,0 +1,18 @@
+package io;
+
+import java.io.*;
+import java.util.*;
+
+public class DirList {
+    public static void main(String[] args) {
+        File path = new File(".");
+        String[] list;
+        if (args.length == 0)
+            list = path.list();
+        else
+            list = path.list(new DirFilter(args[0]));
+        Arrays.sort(list, String.CASE_INSENSITIVE_ORDER);
+        for (String dirItem : list)
+            System.out.println(dirItem);
+    }
+}
---
+++ src/main/java/io/DirList2.java
@@ -0,0 +1,31 @@
+package io;
+
+import java.util.Arrays;
+import java.util.regex.*;
+import java.io.*;
+
+public class DirList2 {
+    public static FilenameFilter filter(final String regex) {
+        return new FilenameFilter() {
+            private Pattern pattern = Pattern.compile(regex);
+            @Override
+            public boolean accept(File dir, String name) {
+                return pattern.matcher(name).matches();
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+        File path = new File(".");
+        String[] list;
+        if (args.length == 0) {
+            list = path.list();
+        } else {
+            list = path.list(filter(args[0]));
+        }
+        Arrays.sort(list);
+        for (String dirItem : list) {
+            System.out.println(dirItem);
+        }
+    }
+}
