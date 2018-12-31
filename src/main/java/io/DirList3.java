1263f84fbf01f43a80ee56c65e0666cd2c75d469
YaMing Wu <wuyaming@gmail.com>
2018-12-31 16:19:41 -0600

    add protobuf tutorial

--- src/main/java/io/DirList3.java
+++ src/main/java/io/DirList3.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.regex.Pattern;
 
 public class DirList3 {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         File path = new File(".");
         String[] list;
         if (args.length == 0) {
